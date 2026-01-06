1b56668c4949c7f62c840431f1f9a8364ab18d1d
霍珽 <thuo@lygeport.gov.cn>
2026-01-06 18:01:46 +0800

    项目初始化

---
+++ springboot-web-quarkstart-spring/src/main/java/com/itheima/SpringbootWebQuarkstartSpringApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebQuarkstartSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebQuarkstartSpringApplication.class, args);
+    }
+
+}
